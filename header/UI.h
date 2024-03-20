2bf7648fe8f269e8f9faa54dc34337d59ec47a0a
JFSLopes <josefilipe3333@gmail.com>
2024-03-20 11:40:33 +0000

    Some UI files

--- header/UI.h
+++ header/UI.h
@@ -1,6 +1,10 @@
 #ifndef DA_FIRSTPROJECT_UI_H
 #define DA_FIRSTPROJECT_UI_H
 
-enum Options {};
+#include <string>
+
+std::string getNewFileName(const std::string& name);
+char getYesNoAnswer();
+void displayChooseDataSet(std::string& reservoirs, std::string& stations, std::string& cities, std::string& pipes, std::string& path);
 
 #endif
--- src/UI.cpp
+++ src/UI.cpp
@@ -1,1 +1,33 @@
 #include "../header/UI.h"
+#include <iostream>
+
+
+std::string getNewFileName(const std::string& name){
+    std::cout << "Enter the new file name for the " << name << ".\n";
+    std::string newFile;
+    std::getline(std::cin, newFile);
+    return newFile;
+}
+
+char getYesNoAnswer(){
+    std::cout << "[y/n]: ";
+    do{
+        std::string value;
+        std::cin >> value;
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        if(value.size() == 1 and (value[0] == 'y' or value[0] == 'n')) return value[0] == 'y';
+        else{
+            std::cout << "Invalid input, enter either 'y' or 'n': ";
+        }
+    } while (true);
+}
+
+void displayChooseDataSet(std::string& reservoirs, std::string& stations, std::string& cities, std::string& pipes, std::string& path){
+    std::cout << "The default files names are the following?\n"
+              << "\t Cities.csv, Reservoir.csv, Pipes.csv, Stations.csv\n";
+    std::cout << "Do you pretend to use them?\n";
+    bool answer = getYesNoAnswer();
+    if (!answer){
+
+    }
+}
